e9eeff247c69fca9e0b93a91ac4d521435bfb74d
Samboja651 <waribekihiko651@gmail.com>
2024-11-08 21:22:53 +0300

    store new products into database

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+# creating database in sqlite3
+DATABASE = "products.db"
+
+def get_db():
+    '''
+    Creates database automatically if it does not exits\n.
+    Creates a product table & defines its attributes\n.
+    Returns a connection obj to the database.
+    '''
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    statement = '''
+                CREATE TABLE IF NOT EXISTS PRODUCT(
+                Id INT AUTO_INCREMENT,
+                Category VARCHAR(20),
+                Name VARCHAR(25),
+                Description VARCHAR(50),
+                Price INT
+                );        
+    '''
+    cursor.execute(statement)
+    conn.commit()
+    conn.close()
+    return conn
+
 
 @app.get("/")
 def home():
@@ -25,6 +52,23 @@ def add_new_product():
         description = request.form["description"]
         price = request.form["price"]
         
+        # STORE THE PRODUCT INTO DB
+        get_db() 
+        # I could just assign conn to get_db() since it returns a 
+        # connection to the database. But if I use conn multilpe times
+        # it will increase the run time because conn has to execute getdb first.
+        # Instead I'm going to create a new connection directly to database.
+
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        store_product = '''
+                        INSERT INTO PRODUCT(Category, Id, Description, Price)
+                        VALUES(?, ?, ?, ?);
+        '''
+        cursor.execute(store_product, (category, name, description, price))
+        conn.commit()
+        conn.close()
+
         return f"{category}, {name}, {description}, {price}"
 
 
